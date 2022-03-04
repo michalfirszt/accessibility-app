1c0349983ae3273e7808ae8563b3b68a802425bb
michalfirszt <michalfiszu@gmail.com>
2022-03-04 17:14:29 +0100

    CI start app

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,5 @@
-const coveragePlugin = require('@cypress/code-coverage/task');
-
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  config = coveragePlugin(on, config);
-  return config;
-};
+module.exports = (on, config) => {};
